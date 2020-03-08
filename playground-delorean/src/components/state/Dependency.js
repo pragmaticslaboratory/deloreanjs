ca892852283f655f6897284d55fddbd3f8e01e9f
GuillermoVictorero <45524909+GuillermoVictorero@users.noreply.github.com>
2020-03-08 18:37:57 -0300

    Hid variables that store functions

--- playground-delorean/src/components/state/Dependency.js
+++ playground-delorean/src/components/state/Dependency.js
@@ -13,26 +13,37 @@ const Dependency = (props) => {
     } = props;
 
     // console.log(name,element)
-
-    return (typeof element == 'object') ?
-        // La dependencia es un objeto
-        <WatchedObject
-            element={element}
-            name={name}
-            type={type}
-            toggleObject={toggleObject}
-            displayedObjectsNames={displayedObjectsNames}
-            displayedObjectsDOM={displayedObjectsDOM}
-        />
-        :
-        // La dependencia no es un objeto
-        <div key={name} className="state-values">
+    if(typeof element == 'function'){
+        // La dependencia es una funcion
+        return <div style={{display: 'none'}} key={name} className="state-values">
             <p>{(type !== 'loop') ? name : name + " (loop)"}</p>
             <p>{(typeof element == 'number' || element) ? element.toString() : "undefined" }</p>
             <TextField
                 id={`input-${name}`}
             />
         </div>
+    }
+    else {
+        return (typeof element == 'object') ?
+            // La dependencia es un objeto
+            <WatchedObject
+                element={element}
+                name={name}
+                type={type}
+                toggleObject={toggleObject}
+                displayedObjectsNames={displayedObjectsNames}
+                displayedObjectsDOM={displayedObjectsDOM}
+            />
+            :
+            // La dependencia no es un objeto
+            <div key={name} className="state-values">
+                <p>{(type !== 'loop') ? name : name + " (loop)"}</p>
+                <p>{(typeof element == 'number' || element) ? element.toString() : "undefined" }</p>
+                <TextField
+                    id={`input-${name}`}
+                />
+            </div>
+    }
 }
 
 export default Dependency;
