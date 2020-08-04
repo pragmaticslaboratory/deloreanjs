f5a0d66f4d22d46af657145d0c160020d0b05e39
txvnt <tomas.velez@alumnos.ucn.cl>
2020-08-04 17:20:46 -0400

    timepoints ui changed

--- src/components/Output/index.js
+++ src/components/Output/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import State from '../State';
+
 import Timepoints from '../Timepoints';
 import './styles.css';
 
 class Output extends Component {
   render() {
     return (
-      <div className="delorean-container">
+      <>
         <Timepoints appStore={this.props.appStore} />
-      </div>
+        <div className="output-timeline-container">asd</div>
+      </>
     );
   }
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,10 @@
 import React, { useCallback } from 'react';
 
-import { State, SegmentedControl, SidebarSection } from '..';
+import { State, SegmentedControl, SidebarSection, Watch } from '..';
 
 import { copyOptions, runtimeOptions } from './constants';
+
+import expandButton from '../../assets/img/expand-button.png';
 import './styles.css';
 
 export default function Sidebar(props) {
@@ -32,7 +34,15 @@ export default function Sidebar(props) {
           title="Runtime mode"
         />
       </SidebarSection>
-      <SidebarSection icon="settings" title="Watch variables"></SidebarSection>
+      <SidebarSection icon="settings" title="Watch variables">
+        <div
+          className="toggle-watch-variables"
+          onClick={(ev) => props.appStore.toggleWatchVariables(ev)}>
+          <p>Watch Variables</p>
+          <img style={{ marginLeft: '8px' }} alt="expand icon" src={expandButton} height="10" />
+          {appStore.state.watchVariablesComboBox && <Watch appStore={appStore} />}
+        </div>
+      </SidebarSection>
       <SidebarSection icon="build" title="State">
         <State appStore={appStore} invokeContinuation={() => alert('HOLA')} />
       </SidebarSection>
