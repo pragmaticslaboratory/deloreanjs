b8194ae3c2477c3a862d4a8a02d94988a25cfbac
Felipe <fruiz.rob@gmail.com>
2020-09-22 23:15:23 -0300

    working in line of timeline

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,10 @@
 import React, { useCallback } from 'react';
 import { State, SegmentedControl, SidebarSection, WatchVariables } from '../';
-
 import { copyOptions, runtimeOptions } from './constants';
 import './styles.css';
-import SimpleBar from 'simplebar-react';
 
 export default function Sidebar(props) {
   const { appStore } = props;
-  const maxHeight = '20em';
 
   const toggleCopyMode = useCallback((value) => {
     appStore.toggleCopy(value);
@@ -19,33 +16,26 @@ export default function Sidebar(props) {
 
   return (
     <div className="sidebar-container">
-      <SimpleBar style={{ maxHeight: maxHeight }}>
-        <SidebarSection icon="settings" title="Settings">
-          <SegmentedControl
-            onChange={toggleCopyMode}
-            options={copyOptions}
-            value={appStore.state.copyStyle}
-            title="Copy mode"
-          />
-          <SegmentedControl
-            onChange={toggleRuntimeMode}
-            options={runtimeOptions}
-            value={appStore.state.implicitTimepoints}
-            title="Runtime mode"
-          />
-        </SidebarSection>
-      </SimpleBar>
-      <SimpleBar style={{ maxHeight: maxHeight }}>
-        <SidebarSection icon="visibility" title="Watch variables">
-          <WatchVariables store={appStore} />
-        </SidebarSection>
-      </SimpleBar>
-
-      <SimpleBar style={{ maxHeight: maxHeight }}>
-        <SidebarSection icon="build" title="State">
-          <State appStore={appStore} />
-        </SidebarSection>
-      </SimpleBar>
+      <SidebarSection icon="settings" title="Settings">
+        <SegmentedControl
+          onChange={toggleCopyMode}
+          options={copyOptions}
+          value={appStore.state.copyStyle}
+          title="Copy mode"
+        />
+        <SegmentedControl
+          onChange={toggleRuntimeMode}
+          options={runtimeOptions}
+          value={appStore.state.implicitTimepoints}
+          title="Runtime mode"
+        />
+      </SidebarSection>
+      <SidebarSection icon="visibility" title="Watch variables">
+        <WatchVariables store={appStore} />
+      </SidebarSection>
+      <SidebarSection icon="build" title="State">
+        <State appStore={appStore} />
+      </SidebarSection>
     </div>
   );
 }
--- src/components/TimepointList/index.js
+++ src/components/TimepointList/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css';
+import SimpleBar from 'simplebar-react';
 
 export default function TimepointList(props) {
   const { selectCurrentTimepoint, state } = props.store;
@@ -11,24 +12,26 @@ export default function TimepointList(props) {
         <span className="material-icons timeline-icon">timeline</span>
         <h5 className="timepoint-list-title">Timepoints</h5>
       </div>
-      <div className="timepoints-buttons">
-        {snapshots.map((snapshot) => {
-          const { timePointId } = snapshot;
-          return (
-            <div
-              className={`timepoint-button ${
-                selectedTimePoint === timePointId && 'timepoint-button-selected'
-              }`}
-              onClick={() => selectCurrentTimepoint(snapshot)}
-              kont={snapshot.timePointId}
-              id={snapshot.timePointId}
-              key={snapshot.timePointId}>
-              <span className="material-icons timepoint-icon">place</span>
-              <span className="timepoint-name">{snapshot.timePointId}</span>
-            </div>
-          );
-        })}
-      </div>
+      <SimpleBar style={{ maxHeight: '100%' }}>
+        <div className="timepoints-buttons">
+          {snapshots.map((snapshot) => {
+            const { timePointId } = snapshot;
+            return (
+              <div
+                className={`timepoint-button ${
+                  selectedTimePoint === timePointId && 'timepoint-button-selected'
+                }`}
+                onClick={() => selectCurrentTimepoint(snapshot)}
+                kont={snapshot.timePointId}
+                id={snapshot.timePointId}
+                key={snapshot.timePointId}>
+                <span className="material-icons timepoint-icon">place</span>
+                <span className="timepoint-name">{snapshot.timePointId}</span>
+              </div>
+            );
+          })}
+        </div>
+      </SimpleBar>
     </div>
   );
 }
