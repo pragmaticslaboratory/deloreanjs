2c880ad52cf656b8ad3f36e664edefe23894ea4a
Felipe <fruiz.rob@gmail.com>
2020-10-26 01:21:46 -0300

    fix simple bar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,5 @@
 import React, { useCallback } from 'react';
-<<<<<<< HEAD
-import { State, SegmentedControl, SidebarSection, WatchVariables } from '../';
-=======
 import { StateContainer, SegmentedControl, SidebarSection, WatchVariables } from '../';
-
->>>>>>> a7c3e5ae2df7ec9a0f41f5d7197065fa1fa0ab1d
 import { copyOptions, runtimeOptions } from './constants';
 import './styles.css';
 
@@ -21,33 +16,27 @@ export default function Sidebar(props) {
 
   return (
     <div className="sidebar-container">
-      <SimpleBar style={{ maxHeight: maxHeight }}>
-        <SidebarSection icon="settings" title="Settings">
-          <SegmentedControl
-            onChange={toggleCopyMode}
-            options={copyOptions}
-            value={appStore.state.copyStyle}
-            title="Copy mode"
-          />
-          <SegmentedControl
-            onChange={toggleRuntimeMode}
-            options={runtimeOptions}
-            value={appStore.state.implicitTimepoints}
-            title="Runtime mode"
-          />
-        </SidebarSection>
-      </SimpleBar>
-      <SimpleBar style={{ maxHeight: maxHeight }}>
-        <SidebarSection icon="visibility" title="Watch variables">
-          <WatchVariables store={appStore} />
-        </SidebarSection>
-      </SimpleBar>
+      <SidebarSection icon="settings" title="Settings">
+        <SegmentedControl
+          onChange={toggleCopyMode}
+          options={copyOptions}
+          value={appStore.state.copyStyle}
+          title="Copy mode"
+        />
+        <SegmentedControl
+          onChange={toggleRuntimeMode}
+          options={runtimeOptions}
+          value={appStore.state.implicitTimepoints}
+          title="Runtime mode"
+        />
+      </SidebarSection>
+      <SidebarSection icon="visibility" title="Watch variables">
+        <WatchVariables store={appStore} />
+      </SidebarSection>
 
-      <SimpleBar style={{ maxHeight: maxHeight }}>
-        <SidebarSection icon="build" title="State">
-          <StateContainer appStore={appStore} />
-        </SidebarSection>
-      </SimpleBar>
+      <SidebarSection icon="build" title="State">
+        <StateContainer appStore={appStore} />
+      </SidebarSection>
     </div>
   );
 }
