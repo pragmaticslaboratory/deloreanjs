52506bba133e62187b925637633878e5c449cf50
txvnt <tomas.velez@alumnos.ucn.cl>
2020-10-26 00:26:30 -0300

    feat: added subdependencies views

--- src/components/StateObject/index.js
+++ src/components/StateObject/index.js
@@ -2,36 +2,64 @@ import React, { useState, useCallback, useMemo } from 'react';
 import './styles.css';
 
 const StateObject = (props) => {
-  const { name, type, element, toggleObject, displayedObjectsDOM, displayedObjectsNames } = props;
+  const { name, type, element, isSubdependency } = props;
   const [expanded, setExpanded] = useState(false);
 
   const onPressExpand = useCallback(() => {
     setExpanded((value) => !value);
   }, []);
 
-  const dependencyType = useMemo(() => {
-    const isArray = Array.isArray(element);
+  const checkDependencyType = useCallback((item) => {
+    const isArray = Array.isArray(item);
     if (isArray) return 'array';
     else {
       if (type === 'loop') return 'loop';
     }
     return 'object';
-  }, [element]);
-
-  const renderSubdependency = useCallback((subdependency, index) => {
-    const [key, value] = subdependency;
-    return (
-      <div className={`state-subdependency-${dependencyType}-container`} style={{ content: '1' }}>
-        <span className="state-subdependency-text">{key}</span>
-      </div>
-    );
   }, []);
+
+  const dependencyType = useMemo(() => checkDependencyType(element), [element]);
+
+  const renderSubdependency = useCallback(
+    (subdependency, index) => {
+      const [key, value] = subdependency;
+      const subdependencyType = checkDependencyType(value);
+      if (subdependencyType === 'array') {
+        return <StateObject name={key} type="array" element={value} isSubdependency />;
+      }
+      return (
+        <div
+          className={`state-subdependency-${dependencyType}-container`}
+          style={{
+            content: '1',
+            marginLeft: isSubdependency && 24,
+            marginTop: isSubdependency && 4,
+          }}>
+          <span className="state-subdependency-text">{key}</span>
+          <span className="state-subdependency-text">{value}</span>
+          <input
+            placeholder="New value"
+            className="state-subdependency-input"
+            type="text"
+            id={`input-${name}-${key}`}
+          />
+        </div>
+      );
+    },
+    [name],
+  );
 
   return (
     // El objeto esta seleccionado
     <div key={name}>
       <div className="state-object-container button" onClick={onPressExpand}>
-        <div className="state-object-left">
+        <div
+          className="state-object-left"
+          style={
+            isSubdependency && {
+              content: '1',
+            }
+          }>
           <p>{name}</p> <p className="state-object-type">[{dependencyType}]</p>
         </div>
         <span
